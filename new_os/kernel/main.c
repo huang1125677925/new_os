eade01d2930738e5fffdb410250505ab9a71c30d
huang1125677925 <1125677925@qq.com>
2022-01-16 10:39:02 +0800

    实现断言

---
+++ new_os/kernel/main.c
@@ -0,0 +1,11 @@
+#include "print.h"
+#include "init.h"
+#include "debug.h"
+int main(void) {
+   put_str("I am kernel\n");
+   init_all();
+   // ASSERT(1==2);
+   asm volatile ("sti");	
+   while(1);
+   return 0;
+}
