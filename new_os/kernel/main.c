314ff173b8afefa11c4e0e8a5a02ceff02ae0dba
huang1125677925 <1125677925@qq.com>
2022-01-22 10:46:29 +0800

    add thread create function

--- new_os/kernel/main.c
+++ new_os/kernel/main.c
@@ -2,17 +2,26 @@
 #include "init.h"
 #include "debug.h"
 #include "memory.h"
+#include "thread.h"
+
+void thread_a(void* arg);
 
 int main(void) {
    put_str("I am kernel\n");
    init_all();
 
-   void* addr = get_kernel_pages(3);
-   put_str("\n get_kernel_page start vaddr is ");
-   put_int((uint32_t)addr);
-   put_str("\n");
-
+   thread_start("thread_a", 31, thread_a, "start thread a");
    // asm volatile ("sti");	
    while(1);
    return 0;
+}
+
+void thread_a(void* arg) {
+   char* para = arg;
+   while (1)
+   {
+      put_str(para);
+      put_str("\n");
+   }
+   
 }
